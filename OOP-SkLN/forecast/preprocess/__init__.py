cac16256f0bdd3a76ba2a0eb8b9e139d72ec5acb
Miguel Trejo Marrufo <49818188+TremaMiguel@users.noreply.github.com>
2020-03-16 20:28:26 -0600

    Update called functions

--- OOP-SkLN/forecast/preprocess/__init__.py
+++ OOP-SkLN/forecast/preprocess/__init__.py
@@ -1,4 +1,4 @@
 from preprocess.process_for_AR import preprocess_AR
 from preprocess.process_for_forecast import preprocess
 
-__all__ = ['preprocess_AR', 'preprocess'] 
+__all__ = ['pp_transforms', 'pp_tests', 'pp_processes','preprocess'] 
