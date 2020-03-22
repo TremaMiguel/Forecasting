b113969cf4ff5f5f8b1763011893e30d51f46557
Miguel Trejo Marrufo <49818188+TremaMiguel@users.noreply.github.com>
2020-03-22 17:21:56 -0600

    Add parallel_process and fix errors

--- OOP-SkLN/forecast/preprocess/__init__.py
+++ OOP-SkLN/forecast/preprocess/__init__.py
@@ -1,4 +1,5 @@
-from preprocess.process_for_AR import preprocess_AR
+from preprocess.process_for_AR import pp_transforms,pp_tests, pp_processes
 from preprocess.process_for_forecast import preprocess
+from preprocess.preprocess_multiprocessing import parallel_process
 
-__all__ = ['pp_transforms', 'pp_tests', 'pp_processes','preprocess'] 
+__all__ = ['pp_transforms', 'pp_tests', 'pp_processes','preprocess','parallel_process'] 
