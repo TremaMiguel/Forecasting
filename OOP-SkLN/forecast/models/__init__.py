076a02b29980ca0e01b8bfdb9af4307dc690cac7
Miguel Trejo Marrufo <49818188+TremaMiguel@users.noreply.github.com>
2020-04-15 21:52:19 -0500

    Add init to call models

---
+++ OOP-SkLN/forecast/models/__init__.py
@@ -0,0 +1,5 @@
+from models.AutoRegressives import AutoRegressives 
+from models.DecisionTrees import EnsembleTrees
+from models.NeuralNetworks import NeuralNetworks
+
+__all__ = ['AutoRegressives', 'EnsembleTrees', 'NeuralNetworks']
