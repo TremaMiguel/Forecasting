133c543412bc02a3c3773669999ba0d301d3f264
Miguel Trejo Marrufo <49818188+TremaMiguel@users.noreply.github.com>
2020-04-15 21:24:40 -0500

    Fix indentation

--- OOP-SkLN/forecast/scripts/__init__.py
+++ OOP-SkLN/forecast/scripts/__init__.py
@@ -1,8 +1,3 @@
 from scripts.AutoRegressiveModels import arima, sem, holt, holt_winters
 
-__all__ = [
-		'arima',
-    'sem',
-    'holt',
-    'holt_winters'
-]
+__all__ = ['arima','sem','holt','holt_winters']
