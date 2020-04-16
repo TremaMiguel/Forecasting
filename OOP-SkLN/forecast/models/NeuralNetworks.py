094a461b580ebeb0d8993c2ccd9a1934395c2396
Miguel Trejo Marrufo <49818188+TremaMiguel@users.noreply.github.com>
2020-04-15 22:31:48 -0500

    Fix errors

--- OOP-SkLN/forecast/models/NeuralNetworks.py
+++ OOP-SkLN/forecast/models/NeuralNetworks.py
@@ -5,9 +5,9 @@ from numpy import concatenate
 from math import sqrt 
 from sklearn.metrics import mean_squared_error
 
-class NeuralNetworks():
+class NeuralNetwork():
     
-    def __init__(self, dt:'pd.DataFrame', target_variable:str, X_train='pd.DataFrame', X_test='pd.DataFrame', y_train='pd.DataFrame', y_test='pd.DataFrame', train = True, *args): 
+    def __init__(self, dt:'pd.DataFrame'=None, target_variable:str=None, X_train:'pd.DataFrame'=None, X_test:'pd.DataFrame'=None, y_train:'pd.DataFrame'=None, y_test:'pd.DataFrame'=None, train = True, *args): 
         if train:
             self.X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
             self.X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
@@ -24,16 +24,16 @@ class NeuralNetworks():
     def nn_model(self, model:str, mdl_params:dict, fit_params:dict, plot:bool, layers:int, units:int, steps:int, loss_function:'tf.keras.layers', optimizer:str, epochs:int):
 
         # TO DO : Add a CNN-NN or bidirectional LSTM and RNN-NN
-    	  if model == 'Stacked-LSTM':
-    		    nn_model = tf.keras.models.Sequential()
-    	
-            # Add layers	
+        if model == 'Stacked-LSTM':
+            nn_model = tf.keras.models.Sequential()
+
+            # Add layers
             for l in range(layers): 
                 nn_model.add(units/l,tf.keras.layers.LSTM(**mdl_params))
-			
+
         # Output Layer
         nn_model.add(tf.keras.layers.Dense(steps))
-        nn_model.compile(loss=loss_function,optimizer = )
+        nn_model.compile(loss=loss_function,optimizer = optimizer)
 
         # Fit model 
         nn_model.fit(self.X_train, self.y_train, **fit_params)
